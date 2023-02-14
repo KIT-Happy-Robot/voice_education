6e83e3c99ff958c50583b6bc52fe8b38f97b18f7
Takinami Osuke <c1254459@st.kanazawa-it.ac.jp>
2023-02-14 19:34:25 +0900

    Add files via upload
    ・wdys_def.pyのみコードを修正した
    ・rcj2019のyamlファイルは読み込むが、question_box.yamlの方は読み込まなかった

--- src/wdys_education/wdys_def.py
+++ src/wdys_education/wdys_def.py
@@ -6,12 +6,19 @@ from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 import roslib
 import yaml
+from happymimi_voice_msgs.srv import TTS
+from happymimi_voice_msgs.srv import SpeechToText
 
+tts_pub = rospy.ServiceProxy('/tts', TTS)
+stt_pub = rospy.ServiceProxy('/stt_server', SpeechToText)
 
+file_path ="voice_common_pkg/config/rcj2019_whatdidyousay_questions.yaml"
 
-file_path = roslib.packages.get_pkg_dir("voice_education") + "/config/" + "question_box.yaml"
+def speechRecog():
+    return stt_pub()
 
-
+def speak(sen):
+    tts_pub(sen)
 
 def make_QA_list():
 
@@ -20,10 +27,22 @@ def make_QA_list():
     with open(file_path) as file:
         obj = yaml.safe_load(file)
         for f in obj:
-            question.append(f["Q"])
-            answer.append(f["A"])
+            question.append(f["q"])
+            answer.append(f["a"])
 
     return question,answer
 
-
-
+def true_false(res,que,ans):
+    max_fuz = 0
+    num = 0
+    for i,q in enumerate(que):
+        fuz = fuzz.ratio(res,q)
+        if fuz > max_fuz:
+            max_fuz = fuz
+            num = i
+    if max_fuz <= 40:
+        return False
+    else:
+        speak('question is' + que[num])
+        speak('answer is' + ans[num])
+        return True
